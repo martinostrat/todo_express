64fae5b647c933016b477717c7f945521ad034e4
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-11 12:52:36 +0200

    Read tasks from a file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 // Templates for views
 const path = require('path');
@@ -7,7 +8,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    // Read data from file
+    fs.readFile('./tasks', 'utf-8', (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        const tasks = data.split("\n");
+        res.render('index', { tasks: tasks });
+    })
+
 });
 
 app.listen(3001, () => {
