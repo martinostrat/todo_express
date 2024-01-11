e2fb3cc02f2228fbad8d8d8893881166e2f36d50
martin.ostrat@khk.ee <martin.ostrat@khk.ee>
2024-01-11 15:01:40 +0200

    Delete single entries

--- index.js
+++ index.js
@@ -15,18 +15,45 @@ const readFile = (filename) => {
                 return;
             }
 
-            const tasks = data.split("\n");
+            const tasks = JSON.parse(data);
             resolve(tasks);
+        })
+    })
+}
+
+const writeFile = (filename, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, data, 'utf-8', err => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            resolve(true);
         })
     })
 }
 
 app.get('/', (req, res) => {
     // Read data from file
-    readFile('./tasks')
+    readFile('./tasks.json')
         .then(tasks => {
             console.log(tasks)
             res.render('index', { tasks: tasks })
+        })
+})
+
+app.get('/delete-task/:taskId', (req, res) => {
+    let deletedTaskId = parseInt(req.params.taskId);
+    readFile('./tasks.json')
+        .then(tasks => {
+            tasks.forEach((task, index) => {
+                if (task.id === deletedTaskId) {
+                    tasks.splice(index, 1)
+                }
+            })
+            data = JSON.stringify(tasks, null, 2);
+            writeFile('tasks.json', data);
+            res.redirect('/');
         })
 })
 
@@ -34,17 +61,25 @@ app.get('/', (req, res) => {
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/', (req, res) => {
-    readFile('./tasks')
+    readFile('./tasks.json')
         .then(tasks => {
-            tasks.push(req.body.tasks);
-            const data = tasks.join("\n");
-            fs.writeFile('./tasks', data, err => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                res.redirect('/');
-            })
+
+            let index;
+            if (tasks.length === 0) {
+                index = 0;
+            } else {
+                index = tasks[tasks.length - 1].id + 1;
+            }
+
+            const newTask = {
+                "id": index,
+                "task": req.body.tasks
+            }
+
+            tasks.push(newTask);
+            data = JSON.stringify(tasks, null, 2);
+            writeFile('tasks.json', data);
+            res.redirect('/');
         })
 })
 
